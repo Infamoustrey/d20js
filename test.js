78473790460a663dd593d565a9387216acd2033d
Trey Miller <Trey_M@heitkamp.corp>
2018-07-27 12:24:57 -0500

    Add Ava Unit Testing

--- test.js
+++ test.js
@@ -1,9 +1,24 @@
+import test from 'ava';
+import d20 from './index';
 
-const d20js = require('./index')
+test('coin flip returns heads or tails', t => {
+    let toss = d20.coin();
+    t.true(toss == 'heads' || toss == 'tails')
+});
 
-console.log('Roll 3 d20s.')
-console.log( d20js.roll(3, 'd20') )
+test('name generator returns name', t => {
+    let name = d20.name('egyptian');
+    t.true( typeof name == 'string' && name.length > 0 )
+})
 
-console.log('Toss a coin, result: ' + d20js.coin())
-
-console.log('Egyptian Name: ' + d20js.name('egyptian') )
+test('roll 5d6', t => {
+    let results = d20.roll(5, 'd6')
+    t.true(
+        results.every(r => {
+            return r.hasOwnProperty('roll') 
+            && r.hasOwnProperty('result') 
+            && typeof r.roll === 'number'
+            && typeof r.result === 'number'
+        })
+    )
+})
